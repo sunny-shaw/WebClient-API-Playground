b93de0239dc265133faded0394356ad23a2dc93c
Sunny Shaw <sunny.shaw@thoughtworks.com>
2020-01-11 01:17:25 +0530

    WebClient hands-on examples

---
+++ src/main/kotlin/com/example/web/client/api/tutorial/model/Employee.kt
@@ -0,0 +1,7 @@
+package com.example.web.client.api.tutorial.model
+
+data class Employee(
+        val id: String?,
+        val name: String,
+        val dept: String
+)
---
+++ src/test/kotlin/com/example/web/client/api/tutorial/TutorialWebClientApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.web.client.api.tutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutorialWebClientApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
